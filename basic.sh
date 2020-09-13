285f6cba2756947f0fe6d115d32f810a1150a290
Ying Wang <yingw787.personal@gmail.com>
2020-09-13 16:24:22 -0400

    Update basic.sh.

--- basic.sh
+++ basic.sh
@@ -8,9 +8,9 @@ OVMF=$VMDIR/firmware
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m 32G \
     -machine q35,accel=kvm \
-    -smp 4,cores=2 \
+    -smp cores=4,threads=8,sockets=4 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
@@ -26,3 +26,5 @@ qemu-system-x86_64 \
     -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file=MyDisk.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
